6b7f85c72cd5caf67b23ae84caba24f3f51a587f
doozles411 <cindy.phelps01@gmail.com>
2022-08-08 17:52:46 -0700

    Begin home page loading logic

--- src/home.js
+++ src/home.js
@@ -1,7 +1,26 @@
+function createDescription() {
+    const description = document.createElement('div');
+    description.classList.add('description');
+    const title = document.createElement('h1');
+    title.textContent = 'Zia Teresa';
+    const descriptText = document.createElement('div');
+    descriptText.classList.add('description-text');
+    const paraTop = document.createElement('p');
+    paraTop.textContent = 'A menu for everyone';
+    const paraBottom = document.createElement('p');
+    paraBottom.textContent = 'Offering a la carte, set menus, and bespoke pizzas';
+    descriptText.appendChild(paraTop);
+    descriptText.appendChild(paraBottom);
+    const welcome = document.createElement('h2');
+    welcome.textContent = 'Benvenuto alla famiglia!';
+    description.appendChild(title);
+    description.appendChild(descriptText);
+    description.appendChild(welcome);
+    return description;
+}
 
-
-function createMainSection() {}
-
-function createFooter() {}
+function createInformation() {
+    
+}
 
 function loadHome() {}
