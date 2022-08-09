598af3145de7750fae378834557cd7b4e8871699
doozles411 <cindy.phelps01@gmail.com>
2022-08-08 17:00:51 -0700

    Create nav link classes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-//import loadHome
-//import loadMenu
-//import loadContact
+import loadPage from './pageLoad';
+import loadHome from './home';
+import loadMenu from './menu';
+import loadContact from './contact';
 
+function navEvents() {
+    const homeLink = document.querySelector('.home-page');
+    const menuLink = document.querySelector('.')
+}
